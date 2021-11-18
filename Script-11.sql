89cdc83592b9aa8990d2039ae670a69f7c982f8a
Jiri Valasek <jorgevalaho@gmail.com>
2021-11-18 18:17:50 +0100

    Přidal jsem větu Cau

--- Script-11.sql
+++ Script-11.sql
@@ -5,3 +5,5 @@ select country , round (population / surface_area,4) as pop_dens
 from countries c 
 where population is not null and surface_area is not null
 and population != 0 and surface_area != 0;
+
+# Pridam vetu Cau
